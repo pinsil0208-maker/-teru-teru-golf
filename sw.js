c8d199730c79dd9f71c6bd9e68957e1480de5fcc
pinsil0208-maker <pinsil0208@gmail.com>
2026-07-05 17:39:58 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const CACHE_NAME='teru-teru-golf-option-v2';const FILES=['./','index.html','manifest.json','sw.js','icon-192.png','icon-512.png','apple-touch-icon.png'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const CACHE_NAME='teru-teru-golf-points-v1';
+const FILES=['./','index.html','manifest.json','icon-192.png','icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
